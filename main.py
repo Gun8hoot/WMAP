43f6b95383e29006049063084ff60e541095396b
Gun8hoot <Gun8hoot@proton.me>
2024-06-15 16:08:31 +0200

    Update some file

--- main.py
+++ main.py
@@ -2,14 +2,13 @@
 
 # Import module
 try:
-    from colorama import Fore, Style
+    from colorama import Fore, Style, Back
 except ValueError:
     pass
 import argparse, os, time
 from scapy.all import *
 import sys 
 from logging import getLogger, ERROR
-
 getLogger("scapy.runtime").setLevel(ERROR)
 
 # General vars
@@ -25,18 +24,9 @@ except FileExistsError:
 
 # Arguments vars
 parser = argparse.ArgumentParser(prog='WMAP',description='NMAP version who hold with a thread')
-ip = parser.add_argument('-iP', '--ip', help="entre the ip address to attack")
+parser.add_argument('-iP', '--ip', help="entre the ip address to attack", dest='ip')
 args = parser.parse_args()
 c_ip = args.ip
-
-# Send ICMP query
-def ping(host):
-    ans, unans = sr(IP(dst=host)/ICMP(), timeout=3)
-    try:
-        ans.summary(lambda s,r: r.sprintf(Fore.RESET + Fore.GREEN + "\n\n    [+] %IP.src% is alive") + Fore.RESET)
-    except KeyboardInterrupt:
-        print(Fore.RED + Style.DIM + "\n[!] The script has been interupt by the user" + Style.RESET_ALL)
-        sys.exit(0)
 
 # Send TCP query
 def all_port(host):
@@ -44,7 +34,8 @@ def all_port(host):
     SYNACK = 0x12
     port = 0
     try:
-        print("\nPort open: ")
+        print(Back.RED+"[?] CTRL+C to stop the script\n"+Back.RESET)
+        print(Fore.LIGHTYELLOW_EX+"Port open: "+Fore.RESET)
         while port != 56535:
             SYNACK_request = sr1(IP(dst=host)/TCP(dport=port, flags="S"), timeout=1, verbose=0)
             if SYNACK_request is not None:
@@ -62,9 +53,10 @@ def all_port(host):
         sys.exit(0)
 
 # Decoration
+os.system('cls' if os.name == 'nt' else 'clear')
 deco = open("decoration.txt", "r")
 print(Fore.LIGHTMAGENTA_EX + deco.read() + "\n" + Fore.RESET)
 
 # Call function
-ping(c_ip)
-all_port(c_ip)
+if __name__ == '__main__':
+    all_port(c_ip)
